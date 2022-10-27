0f6b14fa53295b1fd697d7936d47c8f172551ee2
Nicholas Speedy <nspeedy@F3C019.42adel.org.au>
2022-10-27 13:18:31 +1030

    claptrap fight

--- C03/ex00/main.cpp
+++ C03/ex00/main.cpp
@@ -1,45 +1,5 @@
 #include "ClapTrap.hpp"
 
-
-// int main() {
-// 	ClapTrap	clap("clap");
-// 	std::cout << "//*****************************\\" << std::endl;
-// 	ClapTrap	clop("clop");
-// 	std::cout << "//*****************************\\" << std::endl;
-
-// 	while ((clap->getEnergyPoints() > 0 && clap->getHitPoints() > 0) && (clop->getEnergyPoints() > 0 && clop->getHitPoints() > 0))
-// 	{
-// 		clap.action(clop->getName());
-// 		clop.action(clap->getName());
-// 		if (clap->getEnergyPoints() == 0) 
-// 		{
-// 			std::cout << clap->getName() << " is out of energy points and needs to shutdown for a while" << std::endl;
-// 			delete(clap);
-// 			std::cout << clop->getName() << " is victorious HUZZAH" << std::endl;
-// 		}
-// 		if (clap->getHitPoints() == 0)
-// 		{
-// 			std::cout << clap->getName() << " 3...2...1 💥" << std::endl;
-// 			delete(clap);
-// 			std::cout << clop->getName() << " I am the captain now" << std::endl;
-// 		}
-// 		if (clop->getEnergyPoints() == 0)
-// 		{
-// 			std::cout << clop->getName() << " is out of energy points and needs to shutdown for a while" << std::endl;
-// 			delete(clop);
-// 			std::cout << clap->getName() << " beep boop bop I win" << std::endl;
-// 		}
-// 		if (clop->getHitPoints() == 0)
-// 		{
-// 			std::cout << clap->getName() << " 3...2...1 💥" << std::endl;
-// 			delete(clop);
-// 			std::cout << clop->getName() << " die inferior strain" << std::endl;
-// 		}
-// 	}
-
-// 	return (0);
-// }
-
 int main() {
 	std::string name1 = "CLAP";
 	std::string name2 = "CLOP";
